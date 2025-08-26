a96776da03947eb6605489a8e25aa8b43f4aadc4
Anton Danylchenko <anton.danilchenko@gmail.com>
2025-08-26 16:12:13 +0300

    Add ability to connect to any database

--- riverorm/config.py
+++ riverorm/config.py
@@ -3,11 +3,15 @@ from dataclasses import dataclass
 
 from dotenv import load_dotenv
 
+from riverorm import constants
+
 
 @dataclass
 class Config:
     POSTGRES_DSN: str
     MYSQL_DSN: str
+    DB_TYPE: str
+    DEBUG: bool
 
 
 config: Config
@@ -23,4 +27,6 @@ if "config" not in globals():
             "POSTGRES_DSN", "postgresql://river_user:river_pass@localhost:5432/river_test"
         ),
         MYSQL_DSN=os.getenv("MYSQL_DSN", "mysql://river_user:river_pass@localhost:3306/river_test"),
+        DB_TYPE=os.getenv("DB_TYPE", constants.DEFAULT_DB),
+        DEBUG=os.getenv("DEBUG", "False").lower() in ("true", "1", "yes"),
     )
