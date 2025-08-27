5478209f404fe46f884c9dfb3fb2f00ac54b8da9
Anton Danylchenko <anton.danilchenko@gmail.com>
2025-08-27 11:56:14 +0300

    Add global import of DatabaseRegistry

--- riverorm/db/__init__.py
+++ riverorm/db/__init__.py
@@ -1,3 +1,4 @@
 from .base import BaseDatabase as BaseDatabase
 from .mysql import MySQLDatabase as MySQLDatabase
 from .postgres import PostgresDatabase as PostgresDatabase
+from .registry import DatabaseRegistry as DatabaseRegistry
